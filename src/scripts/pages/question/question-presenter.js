d0cc439490228270b21de08d925e7d45028af177
Viane910 <doktaviane910@gmail.com>
2025-05-25 13:19:50 +0700

    ganti value
    + ganti value

--- src/scripts/pages/question/question-presenter.js
+++ src/scripts/pages/question/question-presenter.js
@@ -7,51 +7,51 @@ export default class QuestionPresenter {
     const question = [
       {
         question: '1: Saya senang memecahkan masalah logika dan hitungan.',
-        options: [{ value: 'A' }, { value: 'B' }, { value: 'C' }, { value: 'D' }, { value: 'E' }],
+        options: [{ value: '1' }, { value: '2' }, { value: '3' }, { value: '4' }, { value: '5' }],
       },
       {
         question: '2: Saya tertarik dengan cara kerja komputer atau teknologi.',
-        options: [{ value: 'A' }, { value: 'B' }, { value: 'C' }, { value: 'D' }, { value: 'E' }],
+        options: [{ value: '1' }, { value: '2' }, { value: '3' }, { value: '4' }, { value: '5' }],
       },
       {
         question: '3: Saya menikmati membaca buku tentang manusia atau psikologi.',
-        options: [{ value: 'A' }, { value: 'B' }, { value: 'C' }, { value: 'D' }, { value: 'E' }],
+        options: [{ value: '1' }, { value: '2' }, { value: '3' }, { value: '4' }, { value: '5' }],
       },
       {
         question: '4: Saya ingin membantu orang lain secara langsung, seperti di bidang kesehatan.',
-        options: [{ value: 'A' }, { value: 'B' }, { value: 'C' }, { value: 'D' }, { value: 'E' }],
+        options: [{ value: '1' }, { value: '2' }, { value: '3' }, { value: '4' }, { value: '5' }],
       },
       {
         question: '5: Saya suka bekerja dengan data atau statistik.',
-        options: [{ value: 'A' }, { value: 'B' }, { value: 'C' }, { value: 'D' }, { value: 'E' }],
+        options: [{ value: '1' }, { value: '2' }, { value: '3' }, { value: '4' }, { value: '5' }],
       },
       {
         question: '6: Saya tertarik pada seni atau desain visual.',
-        options: [{ value: 'A' }, { value: 'B' }, { value: 'C' }, { value: 'D' }, { value: 'E' }],
+        options: [{ value: '1' }, { value: '2' }, { value: '3' }, { value: '4' }, { value: '5' }],
       },
       {
         question: '7: Saya ingin membangun atau merancang sesuatu secara fisik.',
-        options: [{ value: 'A' }, { value: 'B' }, { value: 'C' }, { value: 'D' }, { value: 'E' }],
+        options: [{ value: '1' }, { value: '2' }, { value: '3' }, { value: '4' }, { value: '5' }],
       },
       {
         question: '8: Saya suka berbicara atau menyampaikan ide di depan umum.',
-        options: [{ value: 'A' }, { value: 'B' }, { value: 'C' }, { value: 'D' }, { value: 'E' }],
+        options: [{ value: '1' }, { value: '2' }, { value: '3' }, { value: '4' }, { value: '5' }],
       },
       {
         question: '9: Saya menikmati menganalisis kejadian sosial atau politik.',
-        options: [{ value: 'A' }, { value: 'B' }, { value: 'C' }, { value: 'D' }, { value: 'E' }],
+        options: [{ value: '1' }, { value: '2' }, { value: '3' }, { value: '4' }, { value: '5' }],
       },
       {
         question: '10: Saya senang belajar tentang alam, tumbuhan, atau hewan.',
-        options: [{ value: 'A' }, { value: 'B' }, { value: 'C' }, { value: 'D' }, { value: 'E' }],
+        options: [{ value: '1' }, { value: '2' }, { value: '3' }, { value: '4' }, { value: '5' }],
       },
       {
         question: '11: Saya ingin mengembangkan bisnis atau produk.',
-        options: [{ value: 'A' }, { value: 'B' }, { value: 'C' }, { value: 'D' }, { value: 'E' }],
+        options: [{ value: '1' }, { value: '2' }, { value: '3' }, { value: '4' }, { value: '5' }],
       },
       {
         question: '12: Saya tertarik mengajar dan berbagi pengetahuan.',
-        options: [{ value: 'A' }, { value: 'B' }, { value: 'C' }, { value: 'D' }, { value: 'E' }],
+        options: [{ value: '1' }, { value: '2' }, { value: '3' }, { value: '4' }, { value: '5' }],
       },
     ];
     this.view.displayQuestion(question);
