8256c1eeaf5b7a1cd8bc3a969437954faa26a0b7
Viane910 <doktaviane910@gmail.com>
2025-06-10 16:50:57 +0700

    .

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,7 +2,7 @@ import HomePage from '../pages/home/home-page';
 import AboutPage from '../pages/about/about-page';
 import Login from '../data/login/login';
 import Register from '../data/register/register';
-import QuestionOne from '../pages/Question/question-one';
+import QuestionOne from '../pages/question/question-one';
 import ProfilePage from '../pages/dashboard/profile-page';
 import RiwayatPage from '../pages/riwayat/riwayat-page';
 import RecommendationPage from '../pages/rekomendasi/recommendation-page';
